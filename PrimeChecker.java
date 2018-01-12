1ce77c73ae303a487500feeef568bc675f58544a
Abhinandan Panigrahi <chrzrdx@gmail.com>
2018-01-12 15:47:32 +0530

    fix: only check for divisors until half of the number
    This speeds up our check by half. If 2 divides some integer n, then (n/2) divides n as well. So we don't have to try out all integers from 2 to n.

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -3,13 +3,13 @@ public class PrimeChecker {
     System.out.println(isPrime(5));
   }
 
-  // basic checker, checks for factors from (2 to n-1)
+  // improved checker, checks for factors from (2 to n/2)
   // if found, n is not prime
   public static boolean isPrime(int n) {
-    for (int i = 2; i < n; i++) {
+    for (int i = 2; 2 * i < n; i++) {
       if (n % i == 0)
         return false;
     }
     return true;
- }
+  }
 }
