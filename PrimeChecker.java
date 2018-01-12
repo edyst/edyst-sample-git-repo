f002eede796bdebab65c2553107efb0a97740d5f
Abhinandan Panigrahi <chrzrdx@gmail.com>
2018-01-12 15:52:47 +0530

    refactor: check for factors until square root of the number
    If we list out all of the factors of a number, the square root will always be in the middle. So, we don't need to check any factors beyond that.
    
    Also, all primes are odd except for 2, so we can discard checking for even number factors

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -3,10 +3,13 @@ public class PrimeChecker {
     System.out.println(isPrime(5));
   }
 
-  // improved checker, checks for factors from (2 to n/2)
+  // improved checker, checks for 2 and all odd factors uptil the square root of the number
   // if found, n is not prime
   public static boolean isPrime(int n) {
-    for (int i = 2; 2 * i < n; i++) {
+    //check if n is a multiple of 2
+    if (n % 2 == 0) return false;
+    //if not, then just check the odd factors until the square root of n
+    for (int i = 3; i * i <= n; i += 2) {
       if (n % i == 0)
         return false;
     }
